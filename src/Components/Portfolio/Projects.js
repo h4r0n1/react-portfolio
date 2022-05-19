429e33aa92b0dc270be6337f7004d97ef303ff87
h4r0n1 <mdl.diop97@gmail.com>
2022-05-19 00:00:54 +0000

    Fixed some bugs

--- src/Components/Portfolio/Projects.js
+++ src/Components/Portfolio/Projects.js
@@ -7,7 +7,8 @@ export const projects = [
     title: "Tayeur",
     description: "Order management system for tailors.",
     githubLink: "",
-    deployLink: "http://web.recette.tayeur.com",
+    // deployLink: "http://web.recette.tayeur.com",
+    deployLink: "",
     techno: ["React JS", "Ionic", "Django"],
   },
   {
@@ -16,7 +17,8 @@ export const projects = [
     description:
       "Bakeli Carrière helps you find an internship during or at the end of your training.",
     githubLink: "",
-    deployLink: "https://carriere.bakeli.tech",
+    // deployLink: "https://carriere.bakeli.tech",
+    deployLink: "",
     techno: ["React JS", "Bootstrap", "Laravel"],
   },
   {
@@ -25,7 +27,8 @@ export const projects = [
     description:
       "Bakeli School of Technology's information system created for a better management of students.",
     githubLink: "",
-    deployLink: "https://alter.bakeli.tech",
+    // deployLink: "https://alter.bakeli.tech",
+    deployLink: "",
     techno: ["React JS", "Bootstrap", "Laravel"],
   },
   {
@@ -34,7 +37,8 @@ export const projects = [
     description:
       "Bakeli Store is a digital library created by Bakeli School of Technology's students with applications templates.",
     githubLink: "",
-    deployLink: "https://Store.bakeli.tech",
+    // deployLink: "https://Store.bakeli.tech",
+    deployLink: "",
     techno: ["Wordpress"],
   },
   {
